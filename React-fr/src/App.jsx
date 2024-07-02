dd439359affeaac48432077b566148c4117352f2
SkylakeFreak <utkarshbarde2@gmail.com>
2024-07-02 09:43:05 +0530

    c

--- React-fr/src/App.jsx
+++ React-fr/src/App.jsx
@@ -28,15 +28,19 @@ function App() {
     <Header/>
     <BrowserRouter>
     <Routes>
-    <Route path="/Admin" element={<MainAdmin/>}/>
+      <Route>
+      <Route path="/Admin" element={<MainAdmin/>}/>
+
+      </Route>
+    <Route element={<PrivateRouteAdmin/>}>
+    <Route path="/Admin" element={<MainAdmin userauth1={userauth1} />}/>
+    </Route>
       <Route path="/" element={<Front/>}/>
-      <Route path="/Signin" element={<Signin setuseauth={setuseauth} setcurrentemail={setcurrentemail} currentuser={currentuser} setcurrentuser={setcurrentuser}/>}/>
+      <Route path="/Signin" element={<Signin setuseauth={setuseauth} setuseauth1={setuseauth1} setcurrentemail={setcurrentemail} currentuser={currentuser} setcurrentuser={setcurrentuser}/>}/>
       <Route path="/Signup" element={<Signup/>}/>
       <Route element={<PrivateRoute userauth={userauth}/>}>
       <Route path='/Content' element={<Content currentemail={currentemail} currentuser={currentuser}/>}/>
       <Route path='/NewsData' element={<Newsdata  currentemail={currentemail}/>}/>
-      
-
       </Route>
     </Routes>
     </BrowserRouter>
